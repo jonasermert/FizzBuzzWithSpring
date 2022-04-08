e29064ee6bea92931b15a48ad92a3a53ee1f25bd
Jonas <ermert.jonas@yahoo.de>
2022-04-08 20:12:48 +0200

    Project completed

---
+++ src/test/java/de/jonasermert/fizzbuzz/FizzBuzzApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jonasermert.fizzbuzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzBuzzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
